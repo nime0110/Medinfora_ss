2018a079dc51cb4e5e9674b3abfc57f7e880daef
jjoung-2j <145014345+jjoung-2j@users.noreply.github.com>
2024-06-17 22:53:22 +0900

    [Fix_hj]github 설정중

--- Mediinfora/src/main/java/com/spring/app/HomeController.java
+++ Mediinfora/src/main/java/com/spring/app/HomeController.java
@@ -14,12 +14,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-// @Controller
+@Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/index.bibo", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
