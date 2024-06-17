ec5b65419e0896c040cb2b993fae0321791f36bb
SeoDongHyeok <162319225+Forest-Seo@users.noreply.github.com>
2024-06-17 22:51:57 +0900

    prefix_DH

--- Mediinfora/src/main/java/com/spring/app/HomeController.java
+++ Mediinfora/src/main/java/com/spring/app/HomeController.java
@@ -8,18 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Handles requests for the application home page.
  */
-// @Controller
+@Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/index.bibo")
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
