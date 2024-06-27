48e216ed220e54e1673f4e3d072d84c785cf8937
SeoDongHyeok <162319225+Forest-Seo@users.noreply.github.com>
2024-06-27 12:06:34 +0900

    [Feat dh]
    update login, made by JH

--- Medinfora/src/main/java/com/spring/app/main/controller/MainController.java
+++ Medinfora/src/main/java/com/spring/app/main/controller/MainController.java
@@ -12,8 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.app.main.domain.MemberDTO;
 //import com.spring.app.common.Myutil;
 //import com.spring.app.main.domain.HospitalDTO;
 import com.spring.app.main.service.MainService;
--- Medinfora/src/main/java/com/spring/app/main/service/MainService_imple.java
+++ Medinfora/src/main/java/com/spring/app/main/service/MainService_imple.java
@@ -3,6 +3,7 @@ package com.spring.app.main.service;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
