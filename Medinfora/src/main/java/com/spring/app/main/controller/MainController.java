78b4e2e863dbd2190b7fbb396b561b0290cf4fe6
JIHOON KANG <162292730+exhuni777@users.noreply.github.com>
2024-06-28 17:43:45 +0900

    [Feat_jh] commit
    [Feat_jh] commit

--- Medinfora/src/main/java/com/spring/app/main/controller/MainController.java
+++ Medinfora/src/main/java/com/spring/app/main/controller/MainController.java
@@ -1,10 +1,12 @@
 package com.spring.app.main.controller;
 
+import java.io.PrintWriter;
 import java.util.HashMap;
 //import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -155,80 +157,96 @@ public class MainController {
 	
 	// 카카오 로그인
 	@RequestMapping(value="/login/kakaoLogin.bibo")
-	public void kakaoLogin(HttpServletRequest request) {
+	public void kakaoLogin(HttpServletRequest request, HttpServletResponse response) {
 		
-		String code = request.getParameter("code");
-		// System.out.println(code);
 		
-		String accessToken = KakaoApi.getAccessToken(code);
-		// System.out.println("확인용 accessToken"+accessToken);
+		try {
+			
+			String code = request.getParameter("code");
+			// System.out.println(code);
+			
+			String accessToken = KakaoApi.getAccessToken(code);
+			// System.out.println("확인용 accessToken"+accessToken);
+			
+			Map<String, Object> userInfo = KakaoApi.getUserInfo(accessToken);
+			
+			String name = (String)userInfo.get("name");
+			String email = (String)userInfo.get("email");
+			String is_email_verified = (String)userInfo.get("is_email_verified");
+			String birth_year = (String)userInfo.get("birthyear");
+			String birth_day = (String)userInfo.get("birthday");
+			String phone_number = (String)userInfo.get("phone_number");
+			
+			String userid = email.substring(0, email.indexOf("@"))+"_kakao";
+			
+			// System.out.println(userid);
 		
-		Map<String, Object> userInfo = KakaoApi.getUserInfo(accessToken);
-		
-		String name = (String)userInfo.get("name");
-		String email = (String)userInfo.get("email");
-		String is_email_verified = (String)userInfo.get("is_email_verified");
-		String birth_year = (String)userInfo.get("birthyear");
-		String birth_day = (String)userInfo.get("birthday");
-		String phone_number = (String)userInfo.get("phone_number");
-		
-		String userid = email.substring(0, email.indexOf("@"))+"_kakao";
-		
-		// System.out.println(userid);
+			
+			// 가정은 다음과 같다
+			/*
+				카카오로 로그인 시 카카오계정(이메일형식)에서 @ 기준으로 id 추출해서 _kakao 추가해서 아이디를 인서트한다.으로 가입한 적이 있는지 확인(loginmethod로 select 조건 추가) 검증한다음
+				없으면, 회원가입 폼으로 넘긴다.
+			*/
+			
+			String birthday = birth_year+birth_day;
+			// System.out.println("확인용 birthday : "+birthday);
+			// 확인용 birthday : 19950406
+			
 	
-		
-		// 가정은 다음과 같다
-		/*
-			카카오로 로그인 시 카카오계정(이메일형식)에서 @ 기준으로 id 추출해서 _kakao 추가해서 아이디를 인서트한다.으로 가입한 적이 있는지 확인(loginmethod로 select 조건 추가) 검증한다음
-			없으면, 회원가입 폼으로 넘긴다.
-		*/
-		
-		String birthday = birth_year+birth_day;
-		// System.out.println("확인용 birthday : "+birthday);
-		// 확인용 birthday : 19950406
-		
-
-		// System.out.println("확인용 카카오 연락처 정보 : "+phone_number);
-		// +82 10-xxxx-xxxx
-		
-		phone_number = phone_number.substring(4);
-		
-		StringBuilder sb = new StringBuilder();
-		
-		String[] mobile_arr = phone_number.split("");
-		
-		for(int i=0; i<mobile_arr.length; i++) {
+			// System.out.println("확인용 카카오 연락처 정보 : "+phone_number);
+			// +82 10-xxxx-xxxx
 			
-			if(!"-".equals(mobile_arr[i])) {
-				sb.append(mobile_arr[i]);
+			phone_number = phone_number.substring(4);
+			
+			StringBuilder sb = new StringBuilder();
+			
+			String[] mobile_arr = phone_number.split("");
+			
+			for(int i=0; i<mobile_arr.length; i++) {
+				
+				if(!"-".equals(mobile_arr[i])) {
+					sb.append(mobile_arr[i]);
+				}
+				
+			}// end of for -----
+			
+			String mobile = "0"+sb.toString();
+			
+			// System.out.println("확인용 mobile: "+mobile);
+			// 확인용 mobile: 010xxxxxxxx
+			
+			
+			// service 에 넘겨줄 데이터 loginEnd에 mapper 에서 where 문 사용
+			String clientip = request.getRemoteAddr();
+			String loginmethod = "1";
+			Map<String, String> paraMap = new HashMap<>();
+			paraMap.put("userid", userid);
+			paraMap.put("pwd", "");
+			paraMap.put("clientip", clientip);
+			paraMap.put("loginmethod", loginmethod);
+	
+			
+			// 유저가 가입되어 잇는지 있는지 확인한다.
+			MemberDTO loginuser = service.loginEnd(paraMap, request);
+			
+			
+			if(loginuser != null) {
+				// 가입된 적이 있는 경우 로그인 처리를 해줘야한다.
+				
+				// **** 웹브라우저에 출력하기 시작 **** //
+				// HttpServletResponse response 객체는 전송되어져온 데이터를 조작해서 결과물을 나타내고자 할때 쓰인다.
+			      
+				response.setContentType("text/html; charset=UTF-8");
+			    
+				PrintWriter out = null;
+				out = response.getWriter();
 			}
-			
-		}// end of for -----
+			else {
+				// 가입된 적이 없으므로 회원가입 폼으로 넘겨준다.
+			}
 		
-		String mobile = "0"+sb.toString();
-		
-		// System.out.println("확인용 mobile: "+mobile);
-		// 확인용 mobile: 010xxxxxxxx
-		
-		
-		// service 에 넘겨줄 데이터 loginEnd에 mapper 에서 where 문 사용
-		String clientip = request.getRemoteAddr();
-		String loginmethod = "1";
-		Map<String, String> paraMap = new HashMap<>();
-		paraMap.put("userid", userid);
-		paraMap.put("clientip", clientip);
-		paraMap.put("loginmethod", loginmethod);
-
-		
-		// 유저가 가입되어 잇는지 있는지 확인한다.
-		MemberDTO loginuser = service.loginEnd(paraMap, request);
-		
-		
-		if(loginuser != null) {
-			// 가입된 적이 있는 경우 로그인 처리를 해줘야한다.
-		}
-		else {
-			// 가입된 적이 없으므로 회원가입 폼으로 넘겨준다.
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		
 		
