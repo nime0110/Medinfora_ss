3f53ef7e3036a7a9d53daf25e6698fde79c83bec
nime0110 <nime0110@naver.com>
2024-06-29 16:28:44 +0900

    [Feat_ss] select tag updating related js and ui #14
    사용자 시,도 선택시 업데이트 되는 함수 작성 / 기존코드수정

--- Medinfora/src/main/webapp/WEB-INF/views/hospitalSearch/hospitalSearch.jsp
+++ Medinfora/src/main/webapp/WEB-INF/views/hospitalSearch/hospitalSearch.jsp
@@ -6,7 +6,7 @@
 <link rel="stylesheet" type="text/css" href="<%= ctxPath%>/resources/css/statistics/medistatistic.css" />
 
 <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=a85d4c332f523d2ef9be7ec67b43ff8e&libraries=services"></script>
-<script type="text/javascript" src="<%= ctxPath%>/resources/js/statistics/homeSearch.js"></script>
+<script type="text/javascript" src="<%= ctxPath%>/resources/js/hospitalSearch/hospitalSearch.js"></script>
 
 <style type="text/css">
 	div#title {
@@ -31,13 +31,28 @@
   	}
 </style>
 
+<!-- 중앙 div -->
 <div align="center">
     <div style="font-size: 20pt; margin-bottom: 20px;">우리동네 근처 병원 찾기</div>
+	<div id="searchBox">
+	
+	  <select id="si-do">
+	    <option value="">시/도 선택</option>
+	    <option value="서울특별시">서울특별시</option>
+	    <option value="경기도">경기도</option>
+	  </select>
+	  
+	  <select id="si-gun-gu">
+	    <option value="">시/군/구 선택</option>
+	  </select>
+	  
+	  <input type="text" id="manualLocation" placeholder="위치를 입력하세요 (예: 서울시 강남구)">
+	  <button onclick="searchManualLocation()">검색</button>
+	  
+	</div>
+	<!-- 카카오맵 설정  -->
     <div id="map" style="width:95%; height:600px;"></div>
-</div> 
+</div>    
+    
+<div id="hospital-info"></div>
 
-    <div id="map"></div>
-    <div id="searchBox">
-        <input type="text" id="manualLocation" placeholder="위치를 입력하세요 (예: 서울시 강남구)">
-        <button onclick="searchManualLocation()">검색</button>
-    </div>
--- Medinfora/src/main/webapp/resources/js/hospitalSearch/hospitalSearch.js
+++ Medinfora/src/main/webapp/resources/js/hospitalSearch/hospitalSearch.js
@@ -1,9 +1,9 @@
-var map;
-var markers = [];
+let map;
+let markers = [];
 
 $(function() {
     // 지도 컨테이너와 옵션 설정
-    var mapContainer = document.getElementById('map'),
+    let mapContainer = document.getElementById('map'),
         mapOption = {
             center: new kakao.maps.LatLng(37.566535, 126.9779692), // 초기 중심 좌표 (서울 시청)
             level: 3 // 초기 확대 레벨
@@ -15,9 +15,9 @@ $(function() {
     // HTML5의 geolocation으로 사용자 위치 가져오기
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            var lat = position.coords.latitude, // 위도
+            let lat = position.coords.latitude, // 위도
                 lon = position.coords.longitude; // 경도
-            var locPosition = new kakao.maps.LatLng(lat, lon); // 위치 객체 생성
+            let locPosition = new kakao.maps.LatLng(lat, lon); // 위치 객체 생성
 
             // 현재 위치로 지도 중심 이동
             map.setCenter(locPosition);
@@ -25,8 +25,6 @@ $(function() {
             // 현재 위치에 마커 추가
             addMarker(locPosition, "현재 위치");
 
-            // 주변 병원 검색
-            searchHospitals(locPosition);
         }, function(error) {
             // 위치 정보를 가져오는데 실패한 경우
             alert('위치 정보를 가져올 수 없습니다.');
@@ -36,20 +34,27 @@ $(function() {
         alert('GPS를 지원하지 않습니다');
     }
 
-    // Enter 키로 수동 위치 검색
+    // 검색부분 작성
     $('#manualLocation').on('keydown', function(e) {
         if (e.keyCode == 13) {
             searchManualLocation();
         }
     });
+    
+    //시-도 부분이 바뀌면 업데이트
+    $('#si-do').on('change', function() {
+    	updateSigunGu();
+    });
+
+    
 });
 
 // 주변 병원 검색 함수
 function searchHospitals(locPosition) {
-    var places = new kakao.maps.services.Places();
+    let places = new kakao.maps.services.Places();
     places.categorySearch('HP8', function(result, status) {
         if (status === kakao.maps.services.Status.OK) {
-            for (var i = 0; i < result.length; i++) {
+            for (let i = 0; i < result.length; i++) {
                 displayMarker(result[i]);
             }
         }
@@ -61,14 +66,14 @@ function searchHospitals(locPosition) {
 
 // 검색된 병원을 지도에 마커로 표시하는 함수
 function displayMarker(place) {
-    var marker = new kakao.maps.Marker({
+    let marker = new kakao.maps.Marker({
         map: map,
         position: new kakao.maps.LatLng(place.y, place.x)
     });
 
     // 마커 클릭 이벤트
     kakao.maps.event.addListener(marker, 'click', function() {
-        var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+        let infowindow = new kakao.maps.InfoWindow({zIndex:1});
         infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
         infowindow.open(map, marker);
     });
@@ -79,12 +84,12 @@ function displayMarker(place) {
 
 // 지도에 마커 추가하는 함수
 function addMarker(position, message) {
-    var marker = new kakao.maps.Marker({
+    let marker = new kakao.maps.Marker({
         map: map,
         position: position
     });
 
-    var infowindow = new kakao.maps.InfoWindow({
+    let infowindow = new kakao.maps.InfoWindow({
         content: '<div style="padding:5px;">' + message + '</div>'
     });
     infowindow.open(map, marker);
@@ -95,21 +100,21 @@ function addMarker(position, message) {
 
 // 지도에서 모든 마커를 제거하는 함수
 function removeMarkers() {
-    for (var i = 0; i < markers.length; i++) {
+    for (let i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
     }
     markers = [];
 }
 
-// 수동 위치 입력을 통해 검색하는 함수
+// 수동 위치 입력을 통해 검색하는 함수 start
 function searchManualLocation() {
-    var address = $('#manualLocation').val();
-    var geocoder = new kakao.maps.services.Geocoder();
+    let address = $('#manualLocation').val();
+    let geocoder = new kakao.maps.services.Geocoder();
 
     // 주소로 위치 검색
     geocoder.addressSearch(address, function(result, status) {
         if (status === kakao.maps.services.Status.OK) {
-            var locPosition = new kakao.maps.LatLng(result[0].y, result[0].x); // 검색된 위치 객체 생성
+            let locPosition = new kakao.maps.LatLng(result[0].y, result[0].x); // 검색된 위치 객체 생성
             map.setCenter(locPosition); // 검색된 위치로 지도 중심 이동
             removeMarkers(); // 기존 마커 제거
             addMarker(locPosition, "검색 위치"); // 검색된 위치에 마커 추가
@@ -121,4 +126,25 @@ function searchManualLocation() {
             alert('주소를 찾을 수 없습니다');
         }
     });
+}
+// 수동 위치 입력을 통해 검색하는 함수 end 
+
+// 시 도 선택시 업데이트 되는 함수   start
+function updateSigunGu() {
+	let selectedSido =  $('#si-do').val(); //서울특별시 , 경기도
+	let sigunGuList = { //추후 DB 내용대로
+        '서울특별시': ['강남구', '서초구', '송파구'],
+        '경기도': ['수원시', '성남시', '용인시']
+    };
+    
+    $('#si-gun-gu').empty();
+    $('#si-gun-gu').append('<option value="">시/군/구 선택</option>');
+
+    if (selectedSido) {
+        let guList = sigunGuList[selectedSido]; //'서울특별시' 선택 key => value 강남구,서초구,송파구
+        //console.log(guList); 
+        for (let i = 0; i < guList.length; i++) { 
+            $('#si-gun-gu').append('<option value="' + guList[i] + '">' + guList[i] + '</option>');
+        }
+    }
 }
