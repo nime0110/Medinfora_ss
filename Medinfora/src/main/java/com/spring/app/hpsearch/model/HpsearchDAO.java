d73ebd8e14095de4c9808b13defed1043a2c9d81
nime0110 <nime0110@naver.com>
2024-07-04 14:54:53 +0900

    [Chore_ss] create mapper hpsearch
    hpserach로 mapper 따로 생성 완료

--- Medinfora/src/main/java/com/spring/app/hpsearch/model/HpsearchDAO.java
+++ Medinfora/src/main/java/com/spring/app/hpsearch/model/HpsearchDAO.java
@@ -3,8 +3,12 @@ package com.spring.app.hpsearch.model;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.spring.app.domain.HospitalDTO;
 
+
+@Mapper
 public interface HpsearchDAO {
 
 	List<HospitalDTO> getHospitalList(Map<String, String> paraMap);
