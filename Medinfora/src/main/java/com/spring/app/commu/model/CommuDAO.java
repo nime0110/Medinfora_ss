28e6ff9c5ddbb501e1eae67869bfa4343de02984
nime0110 <nime0110@naver.com>
2024-07-27 20:08:04 +0900

    [Feat_ss] create commuwrite, update service, dao  #37
    커뮤 글쓰기 기능 구현
    글쓰기/리스트 기능관련 service, dao 업데이트

--- Medinfora/src/main/java/com/spring/app/commu/model/CommuDAO.java
+++ Medinfora/src/main/java/com/spring/app/commu/model/CommuDAO.java
@@ -7,12 +7,26 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.spring.app.domain.HospitalDTO;
+import com.spring.app.domain.commu.CommuBoardDTO;
+import com.spring.app.domain.commu.CommuFilesDTO;
 
 
 @Mapper
 public interface CommuDAO {
 
-	
+	String getSeqCommu();
+
+	int add(CommuBoardDTO cbdto);
+
+	void add_File(CommuFilesDTO cfdto);
+
+	List<CommuBoardDTO> getCommuBoardList(Map<String, String> paraMap);
+
+	int getCBListTotalCount(Map<String, String> paraMap);
+
+	List<String> getfileSeqList();
+
+
 
 
 }
--- Medinfora/src/main/java/com/spring/app/commu/service/CommuService.java
+++ Medinfora/src/main/java/com/spring/app/commu/service/CommuService.java
@@ -5,8 +5,28 @@ import java.util.List;
 import java.util.Map;
 
 import com.spring.app.domain.HospitalDTO;
+import com.spring.app.domain.commu.CommuBoardDTO;
+import com.spring.app.domain.commu.CommuFilesDTO;
 
 public interface CommuService {
+
+	// 시퀀스 채번하기
+	String getSeqCommu();
+
+	// 글 등록
+	int add(CommuBoardDTO cbdto);
+
+	// 첨부파일 등록
+	void add_File(CommuFilesDTO cfdto);
+
+	// 커뮤니티 게시판 
+	List<CommuBoardDTO> getCommuBoardList(Map<String, String> paraMap);
+
+	// 커뮤니티 게시판 전체수 
+	int getCBListTotalCount(Map<String, String> paraMap);
+
+	//첨부파일 유무 알기 위해 DB select
+	List<String> getfileSeqList();
 
 
 }
